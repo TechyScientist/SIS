f8512a1949595728aea3f58c89c32e96d2780f2d
Johnny Console <johnny.console@hotmail.com>
2024-04-19 18:53:54 -0400

    Add starting class

---
+++ src/main/java/com/johnnyconsole/sis/screens/SignOnScreen.java
@@ -0,0 +1,22 @@
+package com.johnnyconsole.sis.screens;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SignOnScreen extends Application {
+
+    @Override
+    public void start(Stage ps) {
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(20));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        ps.setScene(new Scene(pane));
+        ps.setTitle("SIS - Sign On");
+        ps.show();
+    }
+}
