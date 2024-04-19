dbb86cca3189df40de537f0aab49079acd45ee1d
Johnny Console <johnny.console@hotmail.com>
2024-04-19 19:14:56 -0400

    Add basic functionality to sign on screen to check credentials

--- src/main/java/com/johnnyconsole/sis/screens/SignOnScreen.java
+++ src/main/java/com/johnnyconsole/sis/screens/SignOnScreen.java
@@ -1,5 +1,7 @@
 package com.johnnyconsole.sis.screens;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.johnnyconsole.sis.database.Database;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -11,7 +13,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import static javafx.geometry.HPos.*;
+import static javafx.scene.input.KeyCode.*;
+import static com.johnnyconsole.sis.session.Session.*;
 
 public class SignOnScreen extends Application {
 
@@ -23,8 +30,8 @@ public class SignOnScreen extends Application {
         pane.setVgap(10);
 
         Label title = new Label("Sign On");
-        TextField username = new TextField();
-        PasswordField password = new PasswordField();
+        TextField tfUsername = new TextField();
+        PasswordField tfPassword = new PasswordField();
         Button signOn = new Button("Sign On"),
                 exit = new Button("Exit");
 
@@ -33,13 +40,58 @@ public class SignOnScreen extends Application {
         signOn.setMaxWidth(Double.MAX_VALUE);
         exit.setMaxWidth(Double.MAX_VALUE);
 
+        signOn.setOnAction(__ -> {
+            if(authenticate(tfUsername.getText(), tfPassword.getText())) {
+                ps.close();
+                System.out.println("OK");
+            }
+            else {
+                System.out.println("Error");
+            }
+        });
+
+        exit.setOnAction(__ -> ps.close());
+
+        pane.setOnKeyPressed(key -> {
+            if(key.getCode() == ENTER) {
+                signOn.fire();
+            }
+            else if(key.getCode() == ESCAPE) {
+                exit.fire();
+            }
+        });
+
         pane.add(title, 0, 0, 2, 1);
-        pane.addRow(1, new Label("Username:"), username);
-        pane.addRow(2, new Label("Password:"), password);
+        pane.addRow(1, new Label("Username:"), tfUsername);
+        pane.addRow(2, new Label("Password:"), tfPassword);
         pane.addRow(3, exit, signOn);
 
         ps.setScene(new Scene(pane));
         ps.setTitle("SIS - Sign On");
         ps.show();
+
+        if(connection == null)
+            connection = Database.connect();
+
+    }
+
+    public boolean authenticate(String txtUsername, String txtPassword) {
+        try {
+            assert connection != null;
+            char[] passwordBytes = txtPassword.toCharArray();
+
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM users WHERE username=?;");
+            stmt.setString(1, txtUsername);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                char[] hashBytes = rs.getString("passwordHash").toCharArray();
+                return BCrypt.verifyer().verify(passwordBytes, hashBytes).verified;
+            }
+
+        } catch(Exception | AssertionError e) {
+            return false;
+        }
+        return false;
     }
 }
