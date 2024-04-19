2edc9d6b9166bc69cbb2a22efcf094386bbd5893
Johnny Console <johnny.console@hotmail.com>
2024-04-19 18:57:04 -0400

    Add Sign On Screen Basic Layout

--- src/main/java/com/johnnyconsole/sis/screens/SignOnScreen.java
+++ src/main/java/com/johnnyconsole/sis/screens/SignOnScreen.java
@@ -3,8 +3,15 @@ package com.johnnyconsole.sis.screens;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import static javafx.geometry.HPos.*;
 
 public class SignOnScreen extends Application {
 
@@ -14,6 +21,22 @@ public class SignOnScreen extends Application {
         pane.setPadding(new Insets(20));
         pane.setHgap(10);
         pane.setVgap(10);
+
+        Label title = new Label("Sign On");
+        TextField username = new TextField();
+        PasswordField password = new PasswordField();
+        Button signOn = new Button("Sign On"),
+                exit = new Button("Exit");
+
+        title.setFont(Font.font(20));
+        GridPane.setHalignment(title, CENTER);
+        signOn.setMaxWidth(Double.MAX_VALUE);
+        exit.setMaxWidth(Double.MAX_VALUE);
+
+        pane.add(title, 0, 0, 2, 1);
+        pane.addRow(1, new Label("Username:"), username);
+        pane.addRow(2, new Label("Password:"), password);
+        pane.addRow(3, exit, signOn);
 
         ps.setScene(new Scene(pane));
         ps.setTitle("SIS - Sign On");
