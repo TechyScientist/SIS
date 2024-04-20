e3a50f74981036baa907a51c3565769e1a869cd2
Johnny Console <johnny.console@hotmail.com>
2024-04-19 21:42:39 -0400

    Add layout for UserManagementPane

---
+++ src/main/java/com/johnnyconsole/sis/panes/UserManagementPane.java
@@ -0,0 +1,30 @@
+package com.johnnyconsole.sis.panes;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class UserManagementPane extends GridPane {
+
+    public UserManagementPane() {
+        setPadding(new Insets(10));
+        setHgap(10);
+        setVgap(10);
+
+        Label title = new Label("User Management");
+        Button add = new Button("Add User"),
+                edit = new Button("Edit User"),
+                remove = new Button("Remove User"),
+                view = new Button("View User List");
+
+        title.setFont(Font.font(20));
+        add.setMaxWidth(Double.MAX_VALUE);
+        edit.setMaxWidth(Double.MAX_VALUE);
+        remove.setMaxWidth(Double.MAX_VALUE);
+        view.setMaxWidth(Double.MAX_VALUE);
+
+        addColumn(0, title, add, edit, remove, view);
+    }
+}
