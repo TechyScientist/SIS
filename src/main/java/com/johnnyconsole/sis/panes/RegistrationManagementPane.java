cca3c31ec3691d3c8a03abed9aa24bcc76a6961b
Johnny Console <johnny.console@hotmail.com>
2024-04-19 21:43:20 -0400

    Add layout for RegistrationManagementPane

---
+++ src/main/java/com/johnnyconsole/sis/panes/RegistrationManagementPane.java
@@ -0,0 +1,28 @@
+package com.johnnyconsole.sis.panes;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class RegistrationManagementPane extends GridPane {
+
+    public RegistrationManagementPane() {
+        setPadding(new Insets(10));
+        setHgap(10);
+        setVgap(10);
+
+        Label title = new Label("User Management");
+        Button add = new Button("Enroll Student"),
+                remove = new Button("Unenroll Student"),
+                view = new Button("View Student Registration");
+
+        title.setFont(Font.font(20));
+        add.setMaxWidth(Double.MAX_VALUE);
+        remove.setMaxWidth(Double.MAX_VALUE);
+        view.setMaxWidth(Double.MAX_VALUE);
+
+        addColumn(0, title, add, remove, view);
+    }
+}
